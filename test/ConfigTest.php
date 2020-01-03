2b5d44ece02c6a865246ef3010d15ccaa5c8e048
Tobias Spigner <tobias@g-portal.com>
2020-01-04 00:50:37 +0100

    add testing

---
+++ test/ConfigTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ortnit\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class ConfigTest extends TestCase
+{
+    /** @test */
+    public function canFindConfigFiles()
+    {
+        $this->assertTrue(true);
+    }
+}
